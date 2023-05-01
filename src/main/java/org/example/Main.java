222521fc6d236335ea167bdce4082381e494fe41
aalicarefer <aali@carefer.co>
2023-05-01 14:10:27 +0300

    Initial commit one

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        fun();
+
+
+    }
+
+
+    public static void fun(){
+        System.out.println("x");
+    }
+
+}
